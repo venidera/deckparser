e509205d2f456ca9bd22b01422c279a75c20e987
Andre E Toscano <andre@venidera.com>
2018-07-20 16:13:29 -0300

    remoção da parte de lambda para adição da descrição do pacote.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 __version__ = '0.0.1'
 
-try:
-    from pypandoc import convert
+long_desc = """Deckparser
 
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
+O Deckparser é um pacote Python que implementa importadores de dados para os arquivos de entrada dos modelos oficiais do sistema elétrico brasileiro.
+Os modelos NEWAVE, DECOMP e DESSEM são os formatos contemplados. Estes modelos são produzidos pelo CEPEL Eletrobrás.
+O arquivo de dados é denominado deck, talvez por herança do nome utilizado para conjunto de cartões de entrada de dados de sistemas computacionais dos anos 70.
+Um deck consiste de um arquivo, geralmente zipado, que mantém arquivos de dados em formato binário e texto simples.
+Os formatos são definidos e estruturados considerando conceitos do Fortran para a definição de dados de entrada. Assim, para os arquivos de dados baseados em texto pode ser realizado o parsing simples para aquisição dos dados.
+Para os arquivos binários é utilizado o pacote Python Numpy, que suporta a abertura e leitura de arquivos de dados binários do Fortran.
+"""
 
 if __name__ == '__main__':
     project_name = "deckparser"
@@ -21,8 +23,8 @@ if __name__ == '__main__':
         keywords="parser deck newave",
         url="https://github.org/venidera/deckparser",
         packages=find_packages(),
-        install_requires=['numpy','unidecode'],
-        long_description=read_md('README.md'),
+        install_requires=['numpy', 'unidecode'],
+        long_description=long_desc,
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
