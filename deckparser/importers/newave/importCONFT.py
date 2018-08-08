03e498f78b978f4f833e2619b90fd0e9a5f172f5
Andre E Toscano <andre@venidera.com>
2018-08-08 01:20:05 -0300

    ajustes de codificação que impediram a serialização para JSON

--- deckparser/importers/newave/importCONFT.py
+++ deckparser/importers/newave/importCONFT.py
@@ -1,11 +1,15 @@
 def importCONFT(fobj):
     CONFT = dict()
-    for i,v in enumerate(fobj):
+    for i, v in enumerate(fobj):
         if (i > 1) and (v.strip() != ""):
-            CodUTE = v[0:6].strip()
-            nome = v[6:20].strip()
-            ssis = v[20:26].strip()
-            exis = v[26:33].strip()
-            classe = v[33:40].strip()
-            CONFT[CodUTE] = {'nome':nome,'ssis':ssis,'exis':exis,'classe':classe}
+            CodUTE = v[0:6].decode('utf-8').strip()
+            nome = v[6:20].decode('utf-8').strip()
+            ssis = v[20:26].decode('utf-8').strip()
+            exis = v[26:33].decode('utf-8').strip()
+            classe = v[33:40].decode('utf-8').strip()
+            CONFT[CodUTE] = {
+                'nome': nome,
+                'ssis': ssis,
+                'exis': exis,
+                'classe': classe}
     return CONFT
--- deckparser/importers/newave/importMANUTT.py
+++ deckparser/importers/newave/importMANUTT.py
@@ -2,9 +2,9 @@ def importMANUTT(fobj,utes):
     MANUTT = dict()
     for i in utes:
         MANUTT[i] = list()
-    for i,v in enumerate(fobj):
+    for i, v in enumerate(fobj):
         if (i > 1) and (v.strip() != ""):
-            CodUTE = v[17:20].strip()
+            CodUTE = v[17:20].decode('utf-8').strip()
             nome = v[20:35].strip()
             unidtermica = v[38:40].strip()
             data = v[40:49].strip()
@@ -13,5 +13,13 @@ def importMANUTT(fobj,utes):
             ano = data[4:8]
             dur = v[49:53].strip()
             pot = v[55:64].strip()
-            MANUTT[CodUTE].append({'nome':nome,'unidtermica':unidtermica,'data':data,'dia':dia,'mes':mes,'ano':ano,'dur':dur,'pot':pot})
-    return MANUTT    
+            MANUTT[CodUTE].append(
+                {'nome': nome,
+                 'unidtermica': unidtermica,
+                 'data': data,
+                 'dia': dia,
+                 'mes': mes,
+                 'ano': ano,
+                 'dur': dur,
+                 'pot': pot})
+    return MANUTT
