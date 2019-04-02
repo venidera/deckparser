daf97804d299d38445916e58b7f923c9abf792e8
João Borsoi Soares <joaoborsoi@oktala.com.br>
2019-04-02 16:26:20 -0300

    ajustes importação par facilitar gestão dos dados

--- deckparser/importers/decomp/importDADGER.py
+++ deckparser/importers/decomp/importDADGER.py
@@ -3,7 +3,7 @@ from logging import info,debug
 def importDADGER(data, reg=None):
     NumPatamares = 3
     DADGER = {
-        'UH': dict(), 'CT': [], 'UE': [], 'DP': [], 'PQ': [], 'IT': [], 'IA': [],
+        'UH': dict(), 'CT': [], 'UE': [], 'DP': dict(), 'PQ': [], 'IT': dict(), 'IA': [],
         'MP': dict(), 'VE': dict(), 'VM': dict(), 'DF': dict(), 'TI': dict(), 'MT': [], 'VI': [],
         'RE': dict(), 'AC': dict()
     }
@@ -31,11 +31,11 @@ def importDADGER(data, reg=None):
             elif id == "UE":
                 DADGER["UE"].append(importUE(line))
             elif id == "DP":
-                DADGER["DP"].append(importDP(line))
+                importDP(line,DADGER["DP"])
             elif id == "PQ":
                 DADGER["PQ"].append(importPQ(line))
             elif id == "IT":
-                DADGER["IT"].append(importIT(line,NumPatamares))
+                importIT(line,NumPatamares,DADGER["IT"])
             elif id == "IA":
                 DADGER["IA"].append(importIA(line,NumPatamares))
             elif id == "TX":
@@ -103,30 +103,31 @@ def importUE(line):
     }
 
 
-def importDP(line):
-    DP = {
-        'Estagio': int(line[4:6].strip()),
-        'CodSubsistema': int(line[9:11].strip()),
+def importDP(line, DP):
+    estagio = int(line[4:6].strip())
+    codSubsistema = int(line[9:11].strip())
+    
+    if estagio not in DP:
+        DP[estagio] = dict()
+    
+    DP[estagio][codSubsistema] = {
         'NumPatamares': int(line[14:15].strip()),
         'Duracao': []
     }
-    
+
     mercado = []
-    
-    for patamar in range(1,DP['NumPatamares']+1):
+    for patamar in range(1,DP[estagio][codSubsistema]['NumPatamares']+1):
         startPos = patamar*20-1
         carga = line[startPos:startPos+10].strip()
         if carga != "":
             carga = float(carga)
             mercado.append(carga)
 
-        DP['Duracao'].append(float(line[startPos+10:startPos+20].strip()))
+        DP[estagio][codSubsistema]['Duracao'].append(float(line[startPos+10:startPos+20].strip()))
         #    DuracaoTotal += Duracao[Patamar-1];
 
     if len(mercado)>0:
-        DP['MMED'] = mercado
-
-    return DP
+        DP[estagio][codSubsistema]['MMED'] = mercado
 
 def importPQ(line):
     return {
@@ -137,19 +138,18 @@ def importPQ(line):
                   float(line[34:39].strip())]
     }
 
-def importIT(line,numPatamares):
-    IT = {
-        'Estagio': int(line[4:6].strip()),
+def importIT(line,numPatamares,IT):
+    estagio = int(line[4:6].strip())
+    IT[estagio] = {
         'GerIt50': [],
         'Mande': []
     }
     for patamar in range(1,numPatamares+1):
-        gerItInit = patamar*10
-        mandeInit = patamar*10
-        IT['GerIt50'].append(float(line[gerItInit:gerItInit+5].strip()))
-        IT['Mande'].append(float(line[mandeInit:mandeInit+5].strip()))
+        gerItInit = 10+patamar*10
+        mandeInit = 15+patamar*10
+        IT[estagio]['GerIt50'].append(float(line[gerItInit:gerItInit+5].strip()))
+        IT[estagio]['Mande'].append(float(line[mandeInit:mandeInit+5].strip()))
         
-    return IT
         
 def importIA(line,numPatamares):
     IA = {
