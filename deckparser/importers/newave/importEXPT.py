7b181e4a06f9e367195503d0603c9c973629b025
João Borsoi Soares <joaoborsoi@oktala.com.br>
2019-04-18 20:28:39 -0300

    ajustes de codificação

--- deckparser/importers/newave/importEXPT.py
+++ deckparser/importers/newave/importEXPT.py
@@ -3,13 +3,13 @@ def importEXPT(fobj,utes):
     for i in utes:
         EXPT[i] = list()
     for i,v in enumerate(fobj):
-        if (i > 1) and (v.strip() != "") and (v[0] == " "):
-            CodUTE = v[0:4].strip()
-            tipo = v[5:11].strip()
-            modif = v[12:20].strip()
-            mi = v[20:23].strip()
-            anoi = v[23:28].strip()
-            mf = v[28:31].strip()
-            anof = v[31:36].strip()
+        if (i > 1) and (v.strip() != ""):
+            CodUTE = v[0:4].decode('utf-8').strip()
+            tipo = v[5:11].decode('utf-8').strip()
+            modif = v[12:20].decode('utf-8').strip()
+            mi = v[20:23].decode('utf-8').strip()
+            anoi = v[23:28].decode('utf-8').strip()
+            mf = v[28:31].decode('utf-8').strip()
+            anof = v[31:36].decode('utf-8').strip()
             EXPT[CodUTE].append({'tipo':tipo,'modif':modif,'mi': mi,'anoi':anoi,'mf':mf,'anof':anof})
     return EXPT
