2566649b7b77a5a754a349b9bb36c83d7c4b51d6
thayzecosta <thayzemartin@gmail.com>
2024-06-26 10:03:22 -0300

    Correção da leitura do DSVAGUA

--- deckparser/importers/newave/importDSVAGUA.py
+++ deckparser/importers/newave/importDSVAGUA.py
@@ -1,20 +1,21 @@
 from deckparser.importers.imputils import line2list
 
 
-def importDSVAGUA(fdata, uhes, dger):
+def importDSVAGUA(fdata, uhes, dger, decktype=False):
     DSVAGUA = dict()
     for i in uhes:
         DSVAGUA[i] = [0.0] * dger['ni']
     cline = 2
     tipo = ''
     valores = [0.0] * dger['ni']
+    inicio = 0
     while fdata[cline].strip() != '9999':
         # print 'fdata[cline] = ',fdata[cline]
         ano = fdata[cline][0:4].strip()
         if ano in str(dger['yph']):
             CodUHE = fdata[cline][5:9].strip()
             # Verificar se eh linha de tipagem, se nao for vai seguir com o tipo anterior.
-            if len(fdata[cline]) > 102:
+            if len(fdata[cline]) > 104:
                 # linha de tipo
                 valores = list()
                 tipo = fdata[cline][102:120].strip()
@@ -26,8 +27,10 @@ def importDSVAGUA(fdata, uhes, dger):
                 mesini = dger['mi']
             else:
                 mesini = 1
-            valores = line2list(dline=fdata[cline][9:95], mi=mesini, ar=ano, mf=12, bloco=7, vlista=valores, dger=dger)
+            valores = line2list(dline=fdata[cline][9:95], mi=mesini, ar=ano,
+                                mf=12, bloco=7, vlista=valores, dger=dger)
             if ano == str(dger['yph'][-1]):
-                DSVAGUA[CodUHE] = list(map(sum, zip(DSVAGUA[CodUHE], valores)))
+                DSVAGUA[CodUHE] = list(
+                    map(sum, zip(DSVAGUA[CodUHE], valores)))
         cline += 1
     return DSVAGUA
